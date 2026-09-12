08cc579d75b455ccc1859633d0db6f376ca3b7a4
rkmoneanddone <123108938+rkmoneanddone@users.noreply.github.com>
2026-09-12 14:01:31 +0530

    Load admin control center styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './features/admin/admin-control-center.css'
 import App from './App.tsx'
 import { AuthProvider } from "./features/auth/AuthProvider";
 
